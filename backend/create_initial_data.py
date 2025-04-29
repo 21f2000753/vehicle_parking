9187232398536f1c68fe4848908a1fd685b6f84d
21f2000753 <21f2000753@study.iitm.ac.in>
2025-04-29 14:27:13 +0000

    Milestone-VP DB-Relationship

---
+++ backend/create_initial_data.py
@@ -0,0 +1,18 @@
+from flask import current_app as app
+from backend.models import db, User
+from werkzeug.security import generate_password_hash
+
+with app.app_context():
+    db.create_all()
+
+    if not User.query.filter_by(role='admin').first():
+        admin_user = User(
+            username='admin',
+            email='admin@parking.com',
+            password=generate_password_hash('admin123'),
+            role='admin',
+            address='Admin Office',
+            pincode='000000'
+        )
+        db.session.add(admin_user)
+        db.session.commit()
